cf28570a764491dcef71a8d4b9a04f934ef2df73
shahin <shahinmirzaei175@live.com>
2022-05-30 16:41:52 +0430

    add request validation to WinnerStore

--- Modules/Game/Http/Controllers/Api/GameController.php
+++ Modules/Game/Http/Controllers/Api/GameController.php
@@ -6,12 +6,12 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Game\Events\NewCodeRequestInserted;
-use Modules\Game\Http\Requests\CodeStoreRequest;
 use Illuminate\Support\Facades\Redis;
+use Modules\Game\Http\Requests\WinnerStoreRequest;
 
 class GameController extends Controller
 {
-    public function setCode(CodeStoreRequest $request)
+    public function setCode(WinnerStoreRequest $request)
     {
         event(new NewCodeRequestInserted($request->mobile,$request->code));
         return response()->json('success');
