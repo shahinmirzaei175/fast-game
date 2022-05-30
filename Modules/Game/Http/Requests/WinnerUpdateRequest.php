b4431ba44b2486b33296b6e74ee54f35f38066e2
shahin <shahinmirzaei175@live.com>
2022-05-30 14:59:10 +0430

    add request validation class for code and winner models

---
+++ Modules/Game/Http/Requests/WinnerUpdateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Game\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WinnerUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'mobile'  =>  'required',
+            'code'  =>  'required',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
